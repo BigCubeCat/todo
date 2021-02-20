ce7b8439852e42c062b631088b1b00bfdae7758b
bigcubecat <mr.bitochkin@yandex.ru>
2021-02-20 21:03:31 +0700

    add notifycation for task

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.15
 require (
 	github.com/charmbracelet/bubbles v0.7.6
 	github.com/charmbracelet/bubbletea v0.12.4
+	github.com/gen2brain/beeep v0.0.0-20200526185328-e9c15c258e28
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/muesli/termenv v0.7.4
+	github.com/spf13/pflag v1.0.5
 	golang.org/x/crypto v0.0.0-20210218145215-b8e89b74b9df // indirect
 	golang.org/x/sys v0.0.0-20210218155724-8ebf48af031b // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
--- main.go
+++ main.go
@@ -2,423 +2,49 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 	"time"
 
-	input "github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/muesli/termenv"
+	"github.com/gen2brain/beeep"
+	"github.com/spf13/pflag"
 )
 
-const (
-	normalMode = iota
-	doneTaskListMode
-	additionalMode
-	editMode
-	helpMode
-
-	usage = `
---Normal Mode--
-
-j - move cursor one line down
-k - move cursor one line up
-a - add a new task(move to additional mode)
-d - remove a task
-e - edit the task name(mode to edit mode)
-h - help(switch to help mode)
-x, enter - mark as done
-t - switch to done tasks list mode
-q - save tasks and close this app
-
---Done Tasks List Mode--
-
-j - move cursor one line down
-k - move cursor one line up
-d - remove a task
-t - switch to normal mode
-x, enter - mark as not done
-q - save tasks and close this app
-
---Additional Mode--
-
-ctrl+q - switch to normal mode
-enter - submit
-
---Edit Mode--
-
-ctrl+q - switch to normal mode
-enter - submit
-
---Help Mode--
-
-q - switch to normal mode
-`
-)
-
-type model struct {
-	cursor       int
-	mode         int
-	latestTaskID int
-	tasks        []*Task
-	doneTasks    []*Task
-
-	newTaskNameInput  input.Model
-	editTaskNameInput input.Model
-}
-
-func initializeModel() tea.Model {
-	tasks, doneTasks, ltID := loadTasksFromRepositoryFile()
-
-	cursor := 0
-	if len(tasks) != 0 {
-		cursor = 1
-	}
-
-	newTaskNameModel := input.NewModel()
-	newTaskNameModel.Placeholder = "New task name..."
-	newTaskNameModel.Focus()
-	editTaskNameModel := input.NewModel()
-	editTaskNameModel.Focus()
-
-	return model{
-		cursor:            cursor,
-		mode:              normalMode,
-		latestTaskID:      ltID,
-		tasks:             tasks,
-		doneTasks:         doneTasks,
-		newTaskNameInput:  newTaskNameModel,
-		editTaskNameInput: editTaskNameModel,
-	}
-}
-
-func (model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch m.mode {
-	case normalMode:
-		return m.normalUpdate(msg)
-	case doneTaskListMode:
-		return m.doneTaskListUpdate(msg)
-	case additionalMode:
-		return m.addingTaskUpdate(msg)
-	case editMode:
-		return m.editTaskUpdate(msg)
-	case helpMode:
-		return m.helpUpdate(msg)
-	}
-
-	return m, nil
-}
-
-func (m model) normalUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "j":
-			if m.cursor < len(m.tasks) {
-				m.cursor++
-			}
-		case "k":
-			if m.cursor > 1 {
-				m.cursor--
-			}
-		case "a":
-			m.mode = additionalMode
-			return m, nil //input.Blink()
-		case "d":
-			if m.cursor == 0 {
-				break
-			}
-			m.tasks = append(m.tasks[:m.cursor-1], m.tasks[m.cursor:]...)
-			if len(m.tasks) == 0 {
-				m.cursor = 0
-			} else {
-				m.cursor = 1
-			}
-		case "e":
-			if m.cursor == 0 {
-				break
-			}
-			m.mode = editMode
-			m.editTaskNameInput.Placeholder = m.tasks[m.cursor-1].Name
-			return m, nil //input.Blink()
-		case "h":
-			m.mode = helpMode
-		case "x", "enter":
-			if m.cursor == 0 {
-				break
-			}
-
-			t := m.tasks[m.cursor-1]
-			t.IsDone = true
-			m.doneTasks = append(m.doneTasks, t)
-			m.tasks = append(m.tasks[:m.cursor-1], m.tasks[m.cursor:]...)
-
-			if len(m.tasks) == 0 {
-				m.cursor = 0
-			} else {
-				m.cursor = 1
-			}
-		case "t":
-			if m.mode == doneTaskListMode {
-				if len(m.tasks) == 0 {
-					m.cursor = 0
-				} else {
-					m.cursor = 1
-				}
-				m.mode = normalMode
-				return m, nil
-			}
-			if len(m.doneTasks) == 0 {
-				m.cursor = 0
-			} else {
-				m.cursor = 1
-			}
-			m.mode = doneTaskListMode
-		case "q", "ctrl+c":
-			m.saveTasks()
-			return m, tea.Quit
-		}
-	}
-
-	return m, nil
-}
-
-func (m model) doneTaskListUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "j":
-			if m.cursor < len(m.doneTasks) {
-				m.cursor++
-			}
-		case "k":
-			if m.cursor > 1 {
-				m.cursor--
-			}
-		case "d":
-			if m.cursor == 0 {
-				break
-			}
-			m.doneTasks = append(m.doneTasks[:m.cursor-1], m.doneTasks[m.cursor:]...)
-			if len(m.doneTasks) == 0 {
-				m.cursor = 0
-			} else {
-				m.cursor = 1
-			}
-		case "t":
-			if len(m.tasks) == 0 {
-				m.cursor = 0
-			} else {
-				m.cursor = 1
-			}
-			m.mode = normalMode
-		case "x", "enter":
-			if m.cursor == 0 {
-				break
-			}
-			t := m.doneTasks[m.cursor-1]
-			t.IsDone = false
-			m.tasks = append(m.tasks, t)
-			m.doneTasks = append(m.doneTasks[:m.cursor-1], m.doneTasks[m.cursor:]...)
-			if len(m.doneTasks) == 0 {
-				m.cursor = 0
-			} else {
-				m.cursor = 1
-			}
-		case "q", "ctrl+c":
-			m.saveTasks()
-			return m, tea.Quit
-		}
-	}
-
-	return m, nil
-}
-
-func (m model) addingTaskUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			m.saveTasks()
-			return m, tea.Quit
-		case "ctrl+q":
-			m.mode = normalMode
-			m.newTaskNameInput.Reset()
-			return m, nil
-		case "enter":
-			if m.newTaskNameInput.Value() == "" {
-				return m, nil
-			}
-			m.latestTaskID++
-			value := m.newTaskNameInput.Value()
-			values := strings.Split(value, "t=")
-			var t time.Time
-			var err error
-			deadlined := false
-			if len(values) == 0 {
-				return m, nil
-				t = time.Now()
-				err = nil
-			} else if len(values) == 2 {
-				lay := "2006-01-02"
-				t, err = time.Parse(lay, values[1])
-				if err != nil {
-					t = time.Now()
-				}
-				deadlined = true
-			}
-			m.tasks = append(m.tasks, &Task{
-				ID:          m.latestTaskID,
-				Name:        values[0],
-				CreatedAt:   time.Now(),
-				HasDeadline: deadlined,
-				Deadline:    t,
-			})
-
-			m.cursor++
-			m.mode = normalMode
-			m.newTaskNameInput.Reset()
-			return m, nil
-		}
-	}
-
-	m.newTaskNameInput, cmd = m.newTaskNameInput.Update(msg)
-
-	return m, cmd
-}
-
-func (m model) editTaskUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			m.saveTasks()
-			return m, tea.Quit
-		case "ctrl+q":
-			m.mode = normalMode
-			m.editTaskNameInput.Reset()
-			return m, nil
-		case "enter":
-			if m.editTaskNameInput.Value() == "" {
-				return m, nil
-			}
-			m.tasks[m.cursor-1].Name = m.editTaskNameInput.Value()
-
-			m.mode = normalMode
-			m.editTaskNameInput.Reset()
-			return m, nil
-		}
-	}
-
-	m.editTaskNameInput, cmd = m.editTaskNameInput.Update(msg)
-
-	return m, cmd
-}
-
-func (m model) helpUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			m.saveTasks()
-			return m, tea.Quit
-		case "q":
-			m.mode = normalMode
-		}
-	}
-
-	return m, nil
-}
-
-func (m model) View() string {
-	switch m.mode {
-	case normalMode, doneTaskListMode:
-		return m.normalView()
-	case additionalMode:
-		return m.addingTaskView()
-	case editMode:
-		return m.editTaskView()
-	case helpMode:
-		return m.helpView()
-	}
-
-	return ""
-}
-
-func (m model) normalView() string {
-	var s string
-	var title termenv.Style
-	var tasksToDisplay []*Task
-	switch m.mode {
-	case normalMode:
-		if len(m.tasks) == 0 {
-			return "You have no tasks. Press 'a' to add your task!\n"
-		}
-		title = termenv.String("TASKS")
-		tasksToDisplay = m.tasks
-	case doneTaskListMode:
-		if len(m.doneTasks) == 0 {
-			return "You have no done tasks.\n"
-		}
-		title = termenv.String("DONE TASKS")
-		tasksToDisplay = m.doneTasks
-	}
-	title = title.Bold().Underline()
-	s = fmt.Sprintf("%v\n\n", title)
-
-	for i, v := range tasksToDisplay {
-		cursor := termenv.String(" ")
-		if m.cursor == i+1 {
-			cursor = termenv.String(">").Foreground(termenv.ANSIYellow)
-		}
-		taskName := termenv.String(v.Name).Foreground(termenv.ANSIBrightGreen)
-		taskName = taskName.Bold()
-		timeLayout := "2006-01-02"
-		start := termenv.String(v.CreatedAt.Format(timeLayout)).Foreground(termenv.ANSIBrightBlue)
-		end := termenv.String(v.Deadline.Format(timeLayout)).Foreground(termenv.ANSIBrightCyan)
-
-		if v.HasDeadline {
-			s += fmt.Sprintf("%v #%d: %s (%s) -> ", cursor, v.ID, taskName, start)
-			s += fmt.Sprintf("(%s) \n", end)
-		} else {
-			s += fmt.Sprintf("%v #%d: %s (%s)\n", cursor, v.ID, taskName, v.CreatedAt.Format(timeLayout))
-		}
-	}
-
-	return s
-}
-
-func (m model) addingTaskView() string {
-	title := termenv.String("Additional Mode").Bold().Underline()
-	return fmt.Sprintf("%v\n\nInput the new task name\n\n%s\n", title, m.newTaskNameInput.View())
-}
-
-func (m model) editTaskView() string {
-	title := termenv.String("Edit Mode").Bold().Underline()
-	return fmt.Sprintf("%v\n\nInput the new task name\n\n%s\n", title, m.editTaskNameInput.View())
-}
-
-func (m model) helpView() string {
-	title := termenv.String("USAGE").Bold().Underline()
-	return fmt.Sprintf("%v"+usage, title)
+func emitSignal(title string, body string, icon string) error {
+	err := beeep.Notify(title, body, icon)
+	return err
 }
 
 func main() {
-	p := tea.NewProgram(initializeModel())
-	if err := p.Start(); err != nil {
-		report(err)
+	var (
+		showHelp      bool
+		checkDeadline bool
+	)
+	pflag.BoolVarP(&showHelp, "help", "h", false, "show help")
+	pflag.BoolVarP(&checkDeadline, "deadline", "d", false, "check deadline")
+	pflag.Parse()
+	if showHelp {
+		pflag.Usage()
+		fmt.Println(usage)
+		return
 	}
-}
-
-func report(err error) {
-	fmt.Printf("todo-cli: %s\n", err.Error())
-	os.Exit(1)
+	if checkDeadline {
+		todos, _, _ := loadTasksFromRepositoryFile()
+		for _, t := range todos {
+			now := time.Now()
+			if t.HasDeadline {
+				fmt.Println("G")
+				if now.Year() == t.Deadline.Year() && now.YearDay() == t.Deadline.YearDay() {
+					fmt.Println("DO")
+					if err := emitSignal("TODO", t.Name, ""); err != nil {
+						panic(err)
+					}
+				}
+			}
+		}
+	} else {
+		p := tea.NewProgram(initializeModel())
+		if err := p.Start(); err != nil {
+			report(err)
+		}
+	}
 }
